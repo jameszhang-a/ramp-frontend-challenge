0570cc6958402fc73e57c9dbca957cbe5d9ca72e
James Zhang <jameszhanga@gmail.com>
2022-05-04 00:34:00 -0500

    Correctly displayes the third option

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,65 +1,33 @@
 // import type { NextPage } from 'next'
 import Head from 'next/head';
 import Image from 'next/image';
-import Triplet from '../components/Triplet';
+import { useState } from 'react';
+import Magic from '../components/Magic';
 import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
+  // list of inputs to be displayed in the Triplet
+  const [ inputs, setInputs ] = useState([
+    false,
+    [ 'item 1', 'item 2', 'item 3' ],
+    {
+      name: 'James Zhang',
+      school: 'University of Wisconsin-Madison',
+      grade: 'Junior'
+    }
+  ]);
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Ramp front end application</title>
+        <title>James Zhang</title>
         <meta name='description' content='Ramp front end application' />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>James Zhang</h1>
-
-        <Triplet input={0} />
-        <Triplet input={[ 'a', 1, 'hello', 'world' ]} />
-        <Triplet input={'James Zhang'} />
-
-        <div className={styles.grid}>
-          <a href='https://nextjs.org/docs' className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href='https://nextjs.org/learn' className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href='https://github.com/vercel/next.js/tree/canary/examples'
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href='https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-          </a>
-        </div>
+        {inputs.map((input, idx) => <Magic key={idx} input={input} />)}
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src='/vercel.svg' alt='Vercel Logo' width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   );
 };
